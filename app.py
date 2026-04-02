6aa3fb7e8b4e9966f038ea6170c17f1fc481ffae
2025btechaimlankit22413-stack <2025btechaimlankit22413@poornima.edu.in>
2026-04-02 19:06:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,7 @@ end = '2026-03-26'
 df = yf.download(user_input, start=start, end=end, progress=False)
 
 # ---------------- DATA CHECK ----------------
-if df is None or df.empty or 'Close' not in df.columns:
-    st.error("⚠️ No valid data found. Check ticker.")
-    st.stop()
+
 
 # ---------------- CLEAN DATA ----------------
 df = df[['Close']].copy()
